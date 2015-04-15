7d653d11374f24d5cc56102011c99e62ea5bfe75
Jonathan Raiman <jonathanraiman@gmail.com>
2015-04-15 12:58:40 -0400

    run.sh no longer calls gulp

--- run.sh
+++ run.sh
@@ -6,10 +6,5 @@ set -e
 set -u
 # handle cascading failures well
 set -o pipefail
-
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-pushd "${SCRIPT_DIR}/dali_visualizer"
-    gulp
-popd
 python3 run_server.py
